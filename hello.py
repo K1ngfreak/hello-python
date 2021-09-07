6a178e0d0b10b32f1dbf65b806d25aa28c266adf
Kingfreak <noahzonneveld@hotmail.com>
2021-09-07 16:20:18 +0200

    changes

--- hello.py
+++ hello.py
@@ -13,16 +13,14 @@ print()
 # Noah Zonneveld
 # 99068856
 
-print(# Hello world from...
-
-    # PPPP   Y     Y   ttttttt   h   h     oooo      nnnn
-    # P   P   Y   Y       t      h   h   o      o    n   n
-    # P   P    Y Y        t      h   h   o      o    n   n
-    # PPPP      Y         t      hhhhh   o      o    n   n
-    # P         Y         t      h   h   o      o    n   n
-    # P         Y         t      h   h   o      o    n   n
-    # P         Y         t      h   h     oooo      n   n
-
-    # Noah Zonneveld
-    # 99068856
-)
+print(#Hello world from...
+    #PPPP   Y     Y   ttttttt   h   h     oooo      nnnn
+    #P   P   Y   Y       t      h   h   o      o    n   n
+    #P   P    Y Y        t      h   h   o      o    n   n
+    #PPPP      Y         t      hhhhh   o      o    n   n
+    #P         Y         t      h   h   o      o    n   n
+    #P         Y         t      h   h   o      o    n   n
+    #P         Y         t      h   h     oooo      n   n
+    #Noah Zonneveld
+    #99068856
+    )
