fb210ee78301ad825edc16e16a5f53959052006b
Kingfreak <noahzonneveld@hotmail.com>
2021-09-07 15:54:50 +0200

    update

---
+++ hello.py
@@ -0,0 +1,14 @@
+print(
+    #Hello world from
+
+    #PPPP   Y     Y   ttttttt   h   h     oooo      nnnn
+    #P   P   Y   Y       t      h   h   o      o    n   n
+    #P   P    Y Y        t      h   h   o      o    n   n
+    #PPPP      Y         t      hhhhh   o      o    n   n
+    #P         Y         t      h   h   o      o    n   n
+    #P         Y         t      h   h   o      o    n   n
+    #P         Y         t      h   h     oooo      n   n
+    
+    #Noah Zonneveld
+    #99068856
+    )
